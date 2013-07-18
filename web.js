0a8c02c0d22c60715e6c8196dc6deadcdee817f9
Ralph Plumley <ralphplumley@gmail.com>
2013-07-18 22:26:01 +0000

    testing web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	 response.send(fs.readFileSync('index.html'));
+	response.send('Cow Goes MOO');
+//	 response.send(fs.readFileSync('index.html'));
 	//response.send('Hello World 2!');
 });
 
