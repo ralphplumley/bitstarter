d8dd7dc6df2dd3d8d6dbae77ff9ffcbcc66eed66
Ralph Plumley <ralphplumley@gmail.com>
2013-07-18 23:00:12 +0000

    Updating web.js

--- web.js
+++ web.js
@@ -9,8 +9,8 @@ buffer.write("Hello from buffer text", "utf-8")
 app.get('/', function(request, response) {
 //	response.send('Cow Goes MOO');
 //	 response.send(fs.readFileSync('index.html'));
-	buffer.tostring('utf-8');
-	//response.send('Hello World 2!');
+//	buffer.tostring('utf-8');
+	response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
