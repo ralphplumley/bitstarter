609f59304c49073c5aaed90453a1257beef1b50f
Ralph Plumley <ralphplumley@gmail.com>
2013-07-19 17:36:16 +0000

    Finalized WK3 HW

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-1#!/usr/bin/env node
+#!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util');
+var rest = require('restler'); 
+var myUrl = "http://infinite-beyond-4907.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,20 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var callback = function(cont) {
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_link>', 'Url Link', clone(assertFileExists))
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    rest.get(myUrl).on('complete', callback);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
